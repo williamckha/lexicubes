4b4f8e621721a0b0131971bbf15da94650dd4928
williamckha <williamha@outlook.com>
2025-05-06 22:19:32 -0700

    Use cn util in Button component

--- frontend/app/components/ui/button.tsx
+++ frontend/app/components/ui/button.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { cn } from "~/lib/utils";
 
 export function Button({ className, ...props }: React.ComponentProps<"button">) {
   return (
     <button
-      className={
-        "flex flex-row justify-center items-center gap-2 w-full border-2 p-2 rounded-full font-medium " +
-        "bg-background hover:enabled:bg-muted enabled:cursor-pointer disabled:cursor-not-allowed disabled:opacity-25 " +
-        className
-      }
+      className={cn(
+        "flex flex-row justify-center items-center gap-2 w-full border-2 p-2 rounded-full font-medium bg-background hover:enabled:bg-muted enabled:cursor-pointer disabled:cursor-not-allowed disabled:opacity-25 ",
+        className,
+      )}
       {...props}
     />
   );
